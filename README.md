4a2a7d619348a1384016bebc4b187cdf39a7feb5
Samuel Šulovský <samuel.sulovsky@gmail.com>
2021-12-26 18:36:56 +0100

    Display demo game list fetched from server

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # IMSE Card Game DB
 TBA.
+
+# How to run
+Run `yarn` to install dependencies, then run `yarn dev` in project root to launch the server and the app.  
+To install dependencies to one or the other use `yarn workspace <app/server> add <package>`.  
---
+++ app/dataLayer/queries.ts
@@ -0,0 +1,40 @@
+import { useQuery } from "react-query"
+import { CardGame, Review } from "../types"
+
+export const useGetCardGames = () => {
+    return useQuery('card-games', async (): Promise<Array<CardGame>> => {
+        // TODO: Extract to environment variable
+        const uri = 'http://localhost:3001/api/games'
+        const response = await fetch(uri).then((res) => res.json())
+
+        return response.map((rawRes: any) => {
+            const baseObj = {
+                id: parseInt(rawRes.id),
+                name: rawRes.name,
+                description: rawRes.description,
+            }
+
+            const reviews = {
+                reviews: rawRes.reviews.map((rawReview: any): Review => {
+                    return {
+                        id: parseInt(rawReview.id),
+                        text: rawReview.text,
+                        rating: parseInt(rawReview.rating),
+                        timestamp: parseInt(rawReview.timestamp),
+                        leftByUser: rawReview.leftByUser,
+                    }
+                }),
+            }
+
+            const verification = rawRes.verification ? {
+                verification: {
+                    comment: rawRes.verification.comment,
+                    timestamp: parseInt(rawRes.verification.timestamp),
+                    verifiedByAdmin: rawRes.verification.verifiedByAdmin,
+                },
+            } : {}
+
+            return {...baseObj, ...reviews, ...verification}
+        })
+    })
+}
