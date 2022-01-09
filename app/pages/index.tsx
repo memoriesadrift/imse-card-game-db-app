98b092466befbb363fb678f42c8dfd1464cfdd38
Samuel Šulovský <samuel.sulovsky@gmail.com>
2022-01-09 20:45:47 +0100

    Add migrate groundwork

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from 'next'
+import { MutationGuard } from '../src/components/Guards'
 import Navbar from '../src/components/Navbar'
-import { usePopulateDatabase } from '../src/dataLayer/mutations'
+import { usePopulateDatabase, useMigrateDatabase } from '../src/dataLayer/mutations'
 
 const Home: NextPage = () => {
   const populateDatabase = usePopulateDatabase()
-
-  const mutationsAreLoading = populateDatabase.isLoading
-  const mutationsHaveError = populateDatabase.isError
-  const mutationsSuccessful = populateDatabase.isSuccess
+  const migrateDatabase = useMigrateDatabase()
 
   return (
     <>      
@@ -15,21 +13,10 @@ const Home: NextPage = () => {
       <h1 className="uk-heading-medium uk-text-center">Home</h1>
         <div className='uk-flex uk-flex-center uk-button-group'>
           <button className="uk-button uk-button-large" onClick={async () => await populateDatabase.mutateAsync()}>Populate Database</button>  
-          <button className="uk-button uk-button-large" onClick={() => {}}>Migrate SQL to NoSQL</button>  
+          <button className="uk-button uk-button-large" onClick={async () => await migrateDatabase.mutateAsync()}>Migrate SQL to NoSQL</button>  
         </div>
-      <p className='uk-flex uk-flex-center'>
-      {(() => {
-        if (mutationsAreLoading) {
-          return 'Loading...'
-        }
-        if (mutationsHaveError) {
-          return 'There was en error executing the request...'
-        }
-        if (mutationsSuccessful) {
-          return 'Successfuly executed your request!'
-        }
-      })()}
-      </p>
+      <MutationGuard {...populateDatabase} />
+      <MutationGuard {...migrateDatabase} />
     </>
   )
 }
