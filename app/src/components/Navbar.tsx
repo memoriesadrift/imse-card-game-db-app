a7035e1299efdd58f19615e6cb9ef4dd027d01bb
Samuel Šulovský <samuel.sulovsky@gmail.com>
2022-01-15 14:33:32 +0100

    Remove unnecessary login page

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { addGame, games, home, login, reports } from '../utils/links'
+import { addGame, games, home, reports } from '../utils/links'
 
 export default function Navbar() {
     return (
@@ -11,7 +11,6 @@ export default function Navbar() {
                     <li><Link href={games}><a>Card Games</a></Link></li>
                     <li><Link href={addGame}><a>Add Game</a></Link></li>
                     <li><Link href={reports}><a>Reports</a></Link></li>
-                    <li><Link href={login}><a>Log In</a></Link></li>
                 </ul>
             </div>
         </nav>
