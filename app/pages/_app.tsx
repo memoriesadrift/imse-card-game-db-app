ef9bca72840da29c3414ab999802fa18cf327e0f
Samuel Šulovský <samuel.sulovsky@gmail.com>
2022-01-18 22:30:41 +0100

    Add footer remove id from edit game

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -13,6 +13,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         <QueryClientProvider client={queryClient}>
           <Component {...pageProps} />
         </QueryClientProvider>
+        <footer>
+            <hr></hr>
+            <div className="uk-width-1-1 uk-text-center" uk-grid="">
+                <div className="uk-card-body uk-width-1-2">
+                <p className="uk-text-break">App created by Samuel Šulovský and Michal Robert Žák for the IMSE course in WS2021/22.</p>
+                </div>
+                <div className="uk-card-body uk-width-1-2">
+                <span className="text-tiny uk-text-break">Card game and card type data provided by the wonderful <a href="https://www.pagat.com" >John McLeod of pagat.com</a></span>
+                </div>
+            </div>
+        </footer>
       </UIKit>
   )
 }
